afc88a88db21ed4c688262d924f86356f1209686
Dev <shayan.dev98@gmail.com>
2019-10-24 21:20:10 +0530

    cloudinary upload

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express")
+const ejs = require('ejs')
+const cloudinary = require("cloudinary");
+require("dotenv").config();
+require("./handlers/cloudinary");
+// const exphbs = require('express-handlebars');
+// app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+// app.set('view engine', 'handlebars');
+
+const app = express();
+
+const upload = require("./handlers/multer");
+
+
+
+app.set('view engine', 'ejs');
+
+
+app.get("/", function (req, res) {
+ res.render('main.ejs')
+})
+
+app.post("/uploads", upload.single("image"), async (req, res) => {
+ const result = await cloudinary.v2.uploader.upload(req.file.path);
+ res.send(result);
+});
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => { console.log(`Server started on ${PORT}`); });
